82ec4f9869b8039843a1a3537ef4c0867859e7d3
Prem Acharya <prem.acharya@hotmail.com>
2018-10-20 12:17:30 +1100

    people cards added

--- src/MainArea.js
+++ src/MainArea.js
@@ -1,15 +1,45 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
+import PeopleCard from "./PeopleCard";
 
 export default class MainArea extends Component {
+  constructor() {
+    super();
+    this.state = {
+      people: [
+        {
+          name: "David the Devil",
+          subtitle: "Corko Inc.",
+          description: "met at a party. Will connet with later"
+        },
+        {
+          name: "David the Devil",
+          subtitle: "Corko Inc.",
+          description: "met at a party. Will connet with later"
+        },
+        {
+          name: "David the Devil",
+          subtitle: "Corko Inc.",
+          description: "met at a party. Will connet with later"
+        },
+        {
+          name: "David the Devil",
+          subtitle: "Corko Inc.",
+          description: "met at a party. Will connet with later"
+        }
+      ]
+    };
+  }
   render() {
+    let peopleCard = this.state.people.map(person => (
+      <Col sm="4">
+        <PeopleCard person={person} />
+      </Col>
+    ));
     return (
       <div>
         <Container fluid>
-          <Row>
-            <Col>Some Text</Col>
-            <Col>Some Text</Col>
-          </Row>
+          <Row>{peopleCard}</Row>
         </Container>
       </div>
     );
